3626f174f82bf1b230b54fb70709eae01de08afd
gray <gray@agora-net.com>
2023-06-07 18:06:35 -0400

    Second test (and it passing too!!!)

---
+++ PdfiumAndroid/src/androidTest/java/io/legere/pdfiumandroid/PdfiumCoreTest.kt
@@ -0,0 +1,55 @@
+package io.legere.pdfiumandroid
+
+import android.util.Log
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class PdfiumCoreTest {
+
+    @Test
+    fun newDocument() {
+        InstrumentationRegistry.getInstrumentation().context
+
+        val pdfBytes = getPdfBytes("f01.pdf")
+
+        assertNotNull(pdfBytes)
+
+        val pdfiumCore = PdfiumCore()
+        val pdfDocument = pdfiumCore.newDocument(pdfBytes)
+
+        assertNotNull(pdfDocument)
+    }
+
+    @Test
+    fun getPageCount() {
+        InstrumentationRegistry.getInstrumentation().context
+
+        val pdfBytes = getPdfBytes("f01.pdf")
+
+        assertNotNull(pdfBytes)
+
+        val pdfiumCore = PdfiumCore()
+        val pdfDocument = pdfiumCore.newDocument(pdfBytes)
+
+        assertNotNull(pdfDocument)
+
+        val pageCount = pdfDocument.getPageCount()
+
+        assert(pageCount == 4) { "Page count should be 4" }
+    }
+
+    private fun getPdfBytes(filename: String) : ByteArray? {
+        val appContext = InstrumentationRegistry.getInstrumentation().context
+        val assetManager = appContext.assets
+        try {
+            val input = assetManager.open(filename)
+            return input.readBytes()
+        } catch (e: Exception) {
+            Log.e(PdfiumCoreTest::class.simpleName, "Ugh",  e)
+        }
+        assetManager.close()
+        return null
+    }
+
+}
