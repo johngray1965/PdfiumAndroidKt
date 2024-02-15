e7c53bb88e210174adc2f429f988b626161009cb
John Gray <1176259+johngray1965@users.noreply.github.com>
2024-02-15 11:39:35 -0500

    use @Synchronized on waitForReady instead of mutex.withLock so we don't need to make it suspend.

--- PdfiumAndroid/src/main/java/io/legere/pdfiumandroid/util/InitLock.kt
+++ PdfiumAndroid/src/main/java/io/legere/pdfiumandroid/util/InitLock.kt
@@ -1,14 +1,10 @@
 package io.legere.pdfiumandroid.util
 
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.util.concurrent.Semaphore
 
 class InitLock {
     private val semaphore = Semaphore(0)
     private var isInitialized = false
-
-    private val mutex = Mutex()
 
     fun markReady() {
         isInitialized = true
@@ -17,11 +13,10 @@ class InitLock {
 
     // We use a mutex to make sure only the
     // first thread waits on the semaphore
-    suspend fun waitForReady() {
-        mutex.withLock {
-            if (!isInitialized) {
-                semaphore.acquire()
-            }
+    @Synchronized
+    fun waitForReady() {
+        if (!isInitialized) {
+            semaphore.acquire()
         }
     }
 }
