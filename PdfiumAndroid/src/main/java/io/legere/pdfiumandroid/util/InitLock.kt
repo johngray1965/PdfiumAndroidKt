53eb170b81b0fe02c4a9dcd2b2c233c1f7d75b7c
John Gray <1176259+johngray1965@users.noreply.github.com>
2024-02-15 11:26:04 -0500

    Update InitLock to handle multiple things calling waitForReady before markReady.

--- PdfiumAndroid/src/main/java/io/legere/pdfiumandroid/util/InitLock.kt
+++ PdfiumAndroid/src/main/java/io/legere/pdfiumandroid/util/InitLock.kt
@@ -1,20 +1,27 @@
 package io.legere.pdfiumandroid.util
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.util.concurrent.Semaphore
 
 class InitLock {
-
     private val semaphore = Semaphore(0)
     private var isInitialized = false
+
+    private val mutex = Mutex()
 
     fun markReady() {
         isInitialized = true
         semaphore.release()
     }
 
-    fun waitForReady() {
-        if (!isInitialized) {
-            semaphore.acquire()
+    // We use a mutex to make sure only the
+    // first thread waits on the semaphore
+    suspend fun waitForReady() {
+        mutex.withLock {
+            if (!isInitialized) {
+                semaphore.acquire()
+            }
         }
     }
 }
