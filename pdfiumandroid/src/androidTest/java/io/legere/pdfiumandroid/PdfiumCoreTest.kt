053d71a0df23808969049ce7c244b2d2acfe8b4a
bvitaliyg <bvitaliyg@gmail.com>
2024-12-27 02:31:43 +0000

    Updated PdfiumCoreTest

--- pdfiumandroid/src/androidTest/java/io/legere/pdfiumandroid/PdfiumCoreTest.kt
+++ pdfiumandroid/src/androidTest/java/io/legere/pdfiumandroid/PdfiumCoreTest.kt
@@ -2,6 +2,7 @@ package io.legere.pdfiumandroid
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.legere.pdfiumandroid.base.BasePDFTest
+import io.legere.pdfiumandroid.base.ByteArrayPdfiumSource
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,6 +17,18 @@ class PdfiumCoreTest : BasePDFTest() {
 
         val pdfiumCore = PdfiumCore()
         val pdfDocument = pdfiumCore.newDocument(pdfBytes)
+
+        assertNotNull(pdfDocument)
+    }
+
+    @Test
+    fun newDocumentWithCustomSource() {
+        val pdfBytes = getPdfBytes("f01.pdf")
+
+        assertNotNull(pdfBytes)
+
+        val pdfiumCore = PdfiumCore()
+        val pdfDocument = pdfiumCore.newDocument(ByteArrayPdfiumSource(pdfBytes!!))
 
         assertNotNull(pdfDocument)
     }
